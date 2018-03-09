13d38a2c663839c698588825371e05c57d4e842b
Tara Anjali Shankar <tara@Taras-MacBook-Air.local>
2018-03-08 23:08:58 -0800

    Updated preprocessing to return numerical training data

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,5 @@
 import numpy as np
-from helperFunctions import isIntegerString, flattenList
+from helperFunctions import isIntegerString, flattenListString
 
 ''' Returns data = [[line1], [line2],...]'''
 def byLine():
@@ -7,11 +7,9 @@ def byLine():
     stripped_data = []
     punctuation = [",", ":", ";", ".", "?", "!", "'"]
 
-
     data = np.loadtxt("data/shakespeare.txt", delimiter='\n', dtype='bytes').astype(str)
 
     # Strip all whitespace (including whitespace on last line of poems)
-    # Remove all punctuation
     for i in range(len(data)):
         line = data[i].strip()
         for punc in punctuation: # Delete punctuation
@@ -30,7 +28,6 @@ def byPoem(flatten=False):
     with open('data/shakespeare.txt') as f:
         poem = []
         for line in f:
-            # delete punctuation
             for punc in punctuation: # Delete punctuation
                 line = line.replace(punc, " ")
 
@@ -41,7 +38,7 @@ def byPoem(flatten=False):
                 poem = poem[:-2] # Get rid of last two newlines
                 poem[len(poem) - 1] = poem[len(poem) - 1].strip() # Get rid of newline at end of poem
                 if flatten:
-                    poem = flattenList(poem)
+                    poem = flattenListString(poem)
                 data.append(poem[:])
                 poem = []
     return data
@@ -57,19 +54,34 @@ def byStanza():
         stanzaData.append(poem[12:])
     return stanzaData
 
+''' Returns training data split by sequences of length seqLength'''
 def getTrainingData(seqLength):
     data = byPoem()
     segments = []
-    trainingX = []
-    trainingY = []
+    trainingXchars = []
+    trainingYchars = []
+    trainingXnums = []
+    trainingYnums = []
 
+    # Get first seqLength characters, and the next character, of each line
     for poem in data:
         for line in poem:
             if len(line) > seqLength:
                 segments.append([line[:seqLength], line[seqLength]])
 
+    # Put the first seqLength characters in trainingXchars and the nex character in traininYchars
     for s in range(len(segments)):
-        trainingX.append(segments[s][0])
-        trainingY.append(segments[s][1])
+        trainingXchars.append(segments[s][0])
+        trainingYchars.append(segments[s][1])
 
-    return (trainingX, trainingY)
+    # Convert the characters in X and Y into numbers
+    characterMap = 'abcdefghijklmnopqrstuvwxyz-() \n'
+
+    for i in range(len(trainingXchars)):
+        trainingXnums.append([characterMap.index(j) for j in trainingXchars[i].lower()])
+    for i in range(len(trainingYchars)):
+        trainingYnums.append([characterMap.index(j) for j in trainingYchars[i].lower()])
+
+    trainingYnums = [y for x in trainingYnums for y in x] # Flatten list
+
+    return (trainingXnums, trainingYnums)
